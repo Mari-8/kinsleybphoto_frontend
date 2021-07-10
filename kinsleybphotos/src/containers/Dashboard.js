67d0467163c915f8d33e1809e37b618d2e6abddd
Mari-8 <ryanaltimari8@gmail.com>
2021-07-10 10:36:57 -0500

    Added conditional render to dashboard

--- kinsleybphotos/src/containers/Dashboard.js
+++ kinsleybphotos/src/containers/Dashboard.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
 
 export default class Dashboard extends Component {
+    
+    state = {
+        loggedIn: false
+    }
+
     render() {
-        return (
-            <div>
-                <h1>Dashboard</h1>
-            </div>
-        )
+        
+        if (this.state.loggedIn) {
+            return (
+                <Container>
+                    <h1>Dashboard</h1>
+                </Container>
+            )
+        } else {
+            return (
+                <Container>
+                    <h1>Login form</h1>
+                </Container>
+            )
+        }
     }
 }
