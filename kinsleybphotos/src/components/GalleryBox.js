d1f31277c251df8cc05734e863d99ef486a36b7f
Mari-8 <ryanaltimari8@gmail.com>
2021-08-10 21:52:23 -0500

    Switched div to a tag, to style as polaroid picture

--- kinsleybphotos/src/components/GalleryBox.js
+++ kinsleybphotos/src/components/GalleryBox.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
 
 
 const GalleryBox = (props) =>
-    <div className="gallery-container">
-       <img src={props.gal.thumbnail} alt="" className="gallery-image" />
-       <div className="middle" >
-         <div className="gallery-text" id={props.gal.id} onClick={() => props.selectGal(props.gal)}>{props.gal.title}</div>
-       </div> 
+  <Container className="text-center">
+    <div className="polaroid">
+      <a title={props.gal.title}>
+        <img height="250" src={props.gal.thumbnail} onClick={() => props.selectGal(props.gal)} id={props.gal.id} alt="gallery-img" title={props.gal.title}/>
+      </a>
     </div>
+  </Container>
+    // <div className="gallery-container">
+    //    <img src={props.gal.thumbnail} alt="" className="gallery-image" />
+    //    <div className="middle" >
+    //      <div className="gallery-text" id={props.gal.id} onClick={() => props.selectGal(props.gal)}>{props.gal.title}</div>
+    //    </div> 
+    // </div>
   
 export default GalleryBox
